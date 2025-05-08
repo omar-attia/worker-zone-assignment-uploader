3cde0cb256a866b990c471d53db7ba899f815444
Omar Attia <o.attia@mawarid.com.sa>
2025-05-08 18:50:50 +0300

    removing not needed models

--- WakeCap.Models/DTOs/AssignmentImportErrorDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WakeCap.Models.DTOs;
-
-public class AssignmentImportErrorDto
-{
-    public int RowNumber { get; set; }
-    public string? Error { get; set; }
-}
--- WakeCap.Models/DTOs/AssignmentImportResultDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WakeCap.Models.DTOs;
-public class AssignmentImportResultDto
-{
-    
-    public int TotalRows { get; set; }
-    public int ValidRows { get; set; }
-    public int InvalidRows => Errors.Count;
-    public List<AssignmentImportErrorDto> Errors { get; set; } = [];
-}
